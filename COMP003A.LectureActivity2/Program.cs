d51e575d2fc6a3a71521f19c48a9190fd9b39163
gonzalez-7 <20agonzalezp@gmail.com>
2026-01-21 23:06:13 -0800

    Add prompt and read second user input

--- COMP003A.LectureActivity2/Program.cs
+++ COMP003A.LectureActivity2/Program.cs
@@ -6,6 +6,9 @@
         {
             Console.Write("Enter the first number: ");
             string input1 = Console.ReadLine();
+
+            Console.Write("Enter the second number: ");
+            string input2 = Console.ReadLine();
         }
     }
 }
