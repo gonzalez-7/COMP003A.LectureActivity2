41e10e07da999f4326d55e2c97b77cdd0e6d6dc9
gonzalez-7 <20agonzalezp@gmail.com>
2026-01-21 23:08:51 -0800

    Parse user inputs into double values

--- COMP003A.LectureActivity2/Program.cs
+++ COMP003A.LectureActivity2/Program.cs
@@ -4,11 +4,18 @@
     {
         static void Main(string[] args)
         {
+            //Step 3
+
             Console.Write("Enter the first number: ");
             string input1 = Console.ReadLine();
 
             Console.Write("Enter the second number: ");
             string input2 = Console.ReadLine();
+
+            // Step 4
+
+            double a = double.Parse(input1);
+            double b = double.Parse(input2);
         }
     }
 }
